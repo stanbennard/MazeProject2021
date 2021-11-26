cf8109f0959d2dd5b36cacf0621080555aa33049
Random Guy <15StannardB.bch@osborne.coop>
2021-11-26 13:27:16 +0000

    grid with green and red square

--- MazeProject/Form1.Designer.cs
+++ MazeProject/Form1.Designer.cs
@@ -28,12 +28,42 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+    }
 }
--- MazeProject/Form1.cs
+++ MazeProject/Form1.cs
@@ -1,10 +1,73 @@
 namespace MazeProject
-{
-	public partial class Form1 : Form
-	{
-		public Form1()
+{ 
+    public partial class Form1 : Form
+    {
+        const int GRID_WIDTH = 80;
+        const int GRID_HEIGHT = 80;
+        const int GRID_SIZE = 5;
+        private System.Windows.Forms.DataGridView myNewGrid;  // Declare a grid for this form
+        //private List<BattleShipRow> battleShipGrid; // Declare this here so that you can use it later to manipulate the cell contents
+        private bool[,] grid = new bool[GRID_WIDTH,GRID_HEIGHT];
+        private int[] player = { 0, 0 };
+        private int[] goal;
+        private Random RNG = new Random();
+
+        public Form1()
 		{
 			InitializeComponent();
-		}
-	}
+            /*myNewGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(myNewGrid)).BeginInit();
+            this.SuspendLayout();
+            myNewGrid.Parent = this;  // You have to set the parent manually so that the grid is displayed on the form
+            myNewGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            myNewGrid.Location = new System.Drawing.Point(10, 10);  // You will need to calculate this postion based on your other controls.  
+            myNewGrid.Name = "myNewGrid";
+            myNewGrid.Size = new System.Drawing.Size(400, 400);  // You said you need the grid to be 12x12.  You can change the size here.
+            myNewGrid.TabIndex = 0;
+            myNewGrid.ColumnHeadersVisible = true; // You could turn this back on if you wanted, but this hides the headers that would say, "Cell1, Cell2...."
+            myNewGrid.RowHeadersVisible = true;
+            myNewGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            myNewGrid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            //myNewGrid.CellClick += MyNewGrid_CellClick;  // Set up an event handler for CellClick.  You handle this in the MyNewGrid_CellClick method, below
+            ((System.ComponentModel.ISupportInitialize)(myNewGrid)).EndInit();
+            this.ResumeLayout(false);
+            myNewGrid.Visible = true;
+            //LoadGridData();*/
+            goal = new int[] { RNG.Next(GRID_WIDTH / 2, GRID_WIDTH - 1), RNG.Next(GRID_HEIGHT / 2, GRID_HEIGHT - 1) };
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for(int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (x == goal[0] && y == goal[1])
+                        grid[x, y] = true;
+                    else if (x == player[0] && y == player[1])
+                        grid[x, y] = true;
+                    else
+                        grid[x, y] = false;
+                }
+            }
+            timer1.Enabled = true;            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(b);
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if(grid[x, y])
+                        g.FillRectangle(Brushes.White, x*GRID_SIZE, y*GRID_SIZE, GRID_SIZE, GRID_SIZE);
+                    else
+                        g.FillRectangle(Brushes.Black, x*GRID_SIZE, y*GRID_SIZE, GRID_SIZE, GRID_SIZE);
+                    if(x == goal[0] && y == goal[1])
+                        g.FillRectangle(Brushes.Red, x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+                    if (x == player[0] && y == player[1])
+                        g.FillRectangle(Brushes.Green, x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+                }
+            }
+            pictureBox1.Image = b;
+        }
+    }
 }
